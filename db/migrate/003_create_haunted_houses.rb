6b70294809dacbeef4d591d65ee0e40f48db206b
grant smith <grantsmith1256@gmail.com>
2019-01-23 21:18:37 +0000

    Done.

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,18 @@
 # Create your haunted_houses migration here
+class HauntedHouses < ActiveRecord::Migration[5.2]
+
+  def change
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :location
+      t.string :theme
+      t.float :price
+      t.boolean :whether_theyre_family_friendly_or_not
+      opening date
+      closing date
+      long description
+    end
+  end
+
+
+end
