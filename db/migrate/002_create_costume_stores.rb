a12aacef543c04d91983fb8558c8894e138c5bb0
grant smith <grantsmith1256@gmail.com>
2019-01-23 20:28:27 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,14 @@
 # Create your costume_stores migration here
 class CreateCostumesStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |t|
+      t.string :name
+      t.string :location
+      t.integer :number_of_costumes
+      t.integer :number_of_employees
+      t.string :whether_or_not_its_still_in_business
+      t.integer :opening time
+      t.integer :closing time
+    end
+  end
 end
