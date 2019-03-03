52c28130956ef17abe9784ade77621c7d2d36877
Brandon Teich <brandonteich123@gmail.com>
2019-03-03 19:38:47 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -2,7 +2,8 @@ source "https://rubygems.org"
 
 gem "rspec"
 gem "activerecord"
-gem "sqlite3"
+gem 'sqlite3', '~> 1.3.6'
 gem "rake"
 gem "database_cleaner"
 gem "pry"
+
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,17 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.0]
+  
+  def change
+    create_table :haunted_houses do |x|
+      x.string :name
+      x.string :location
+      x.string :theme
+      x.integer :price
+      x.boolean :family_friendly
+      x.date :opening_date
+      x.date :closing_date
+      x.text :description
+    end
+  end
+  
+end 
